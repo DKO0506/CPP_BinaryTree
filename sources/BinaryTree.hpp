c5eb34e6788d54dd9a1b3026c7fcbd4b3c759b37
david <davidkokia@gmail.com>
2021-05-26 15:17:08 +0300

    final version 1

---
+++ sources/BinaryTree.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <map>
+
+namespace ariel {
+
+    template<typename T> class BinaryTree {
+        // using iterator = typename std::list<T>::iterator;
+
+        struct Node {
+            T value;
+            Node *right_child;
+            Node *left_child;
+            Node *parent;
+            Node(T v): value(v), right_child(nullptr), left_child(nullptr){}
+        };
+
+        class tree_iterator {
+        private:
+            Node* current_node;
+        public:
+            tree_iterator(Node* v = nullptr):current_node(v) {}
+
+            tree_iterator& operator++ (){
+                return *this;
+            }
+            tree_iterator operator++ (int a){
+                tree_iterator t{current_node};
+                return t;
+            }
+
+            bool operator!= (const tree_iterator& other){return false;}
+            const T& operator* () const {return current_node->value;}
+            const T* operator->() const { return &(current_node->value); }
+
+        };
+
+    protected:
+        Node* root;
+        std::map<T, Node*> vertex;
+
+    public:
+        /**
+         * add_root - add the head of the tree
+        */
+        BinaryTree& add_root(T root){return *this;}
+        /**
+         * add_right - add the right child to some node in the tree
+        */
+        BinaryTree& add_right(T node, T rChild){return *this;}
+        /**
+         * add_left - add the left child to some node in the tree
+        */
+        BinaryTree& add_left(T node, T lChild){return *this;}
+        /**
+         * postorder
+        */
+        tree_iterator begin_postorder(){return tree_iterator{root};}
+        tree_iterator end_postorder(){return nullptr;}
+        /**
+         * preorder
+        */
+        tree_iterator begin_preorder(){return tree_iterator{root};}
+        tree_iterator end_preorder(){return nullptr;}
+        /**
+         * inorder
+        */
+        tree_iterator begin_inorder(){return tree_iterator{root};}
+        tree_iterator end_inorder(){return nullptr;}
+
+        /**
+         * iter normal
+        */
+        tree_iterator begin(){return tree_iterator{root};}
+        tree_iterator end(){return nullptr;}
+
+        friend std::ostream& operator<<(std::ostream& out, BinaryTree<T> const &tree){
+            return out << "Printing tree" << std::endl;}
+    };
+};
